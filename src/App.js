3708b860f2dbb5f60bd561d7aa211ad811139650
michaels90up <michael1990spw@gmail.com>
2022-09-14 13:55:00 +0200

    Create base format

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import './App.css';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React-Meme-Generator</h1>
+      <p>Here should the memes appear.</p>
     </div>
   );
 }
